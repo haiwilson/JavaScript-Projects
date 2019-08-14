3328f2f698f1fd758b7f4cf61791a254b31adf3f
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-08-13 20:49:41 -0400

    push of basic html 3 and 4 into github repository

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_4.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+function my Dictionary() {
+var Animal = {	
+	Species : "Dog" ,
+	Color: "B1ack" , 
+	Breed : "Labrador", 
+	Age : 5,
+	Sound : "Bark!"
+	};
+document.getElementById("Dictionary").innerHTML = Animal.Sound; 
+|
